f7f8973e6358ff3724c5a5cde532c02464373c55
Ruslan Littlewell <mrlittlewell@gmail.com>
2019-11-28 14:59:05 +0300

    update

--- js/init.js
+++ js/init.js
@@ -3,12 +3,12 @@
 
     $('.button-collapse').sideNav();
     $('.carousel').carousel({
-      indicators: true,
+      indicators: false,
     });
-    $('.next').click(function() {
+    $('.next').click(function () {
       $('.carousel').carousel('next');
     });
-    $('.prev').click(function() {
+    $('.prev').click(function () {
       $('.carousel').carousel('prev');
     });
     $('#loader').hide();
@@ -20,23 +20,24 @@
 
     if ($(window).width() >= 960) {
       var lastScrollTop = 0;
-      $(window).scroll(function(event){
-      var st = $(this).scrollTop();
-      if (st > lastScrollTop){
-        $('.navbar-fixed').attr("style","opacity: 0; transition: .5s; top: -100px;");
-      } else {
-        $('.navbar-fixed').attr("style","opacity: 1; transition: .5s; top: 0;").fadeIn(500);
-      }
-      lastScrollTop = st;
-    });
+      $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop) {
+          $('.navbar-fixed').attr("style", "opacity: 0; transition: .5s; top: -100px;");
+        } else {
+          $('.navbar-fixed').attr("style", "opacity: 1; transition: .5s; top: 0;").fadeIn(500);
+        }
+        lastScrollTop = st;
+      });
     }
+    M.updateTextFields();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 $('.collapsible li a').click(function () {
   let costValue = +$('.collapsible li.active .cost').html()
   let planName = $('section.site-main-banner h2').html()
-  let dayCount= $('.collapsible li.active .day-plan').html()
+  let dayCount = $('.collapsible li.active .day-plan').html()
 
   let currentCost = {
     planName: planName,
@@ -49,12 +50,12 @@ $('.collapsible li a').click(function () {
 })
 
 $('.lower-inner').hover(
-    function () {
-      $(this).addClass('hover')
-    },
-    function () {
-      $(this).removeClass('hover')
-    }
+  function () {
+    $(this).addClass('hover')
+  },
+  function () {
+    $(this).removeClass('hover')
+  }
 );
 $('.our-program-section').hover(
   function () {
@@ -80,51 +81,59 @@ $('.lower-inner a').click(function () {
   document.location.replace("/cart.html");
 })
 
-$('#order-form').submit(function(event) {
+$('#order-form').submit(function (event) {
 
   event.preventDefault();
 
   $.ajax({
-      type: 'post',
-      url: $(this).attr('action'),
-      data: new FormData(this),
-      contentType: false,
-      cache: false,
-      processData: false,
-      success: function(result) {
-          $('#order-form input').val("");
-          $('#thanks').modal('open');
-          localStorage.clear();
-      },
+    type: 'post',
+    url: $(this).attr('action'),
+    data: new FormData(this),
+    contentType: false,
+    cache: false,
+    processData: false,
+    success: function (result) {
+      $('#order-form input').val("");
+      $('#thanks').modal('open');
+      localStorage.clear();
+    },
   });
 });
-$('#form-consult').submit(function(event) {
+$('#form-consult').submit(function (event) {
 
   event.preventDefault();
 
   $.ajax({
-      type: 'post',
-      url: $(this).attr('action'),
-      data: new FormData(this),
-      contentType: false,
-      cache: false,
-      processData: false,
-      success: function(result) {
-          $('#form-consult input').val("");
-          $('#thanks').modal('open');
-          localStorage.clear();
-      },
+    type: 'post',
+    url: $(this).attr('action'),
+    data: new FormData(this),
+    contentType: false,
+    cache: false,
+    processData: false,
+    success: function (result) {
+      $('#form-consult input').val("");
+      $('#thanks').modal('open');
+      localStorage.clear();
+    },
+  });
+});
+$('#callback form').submit(function (event) {
+
+  event.preventDefault();
+
+  $.ajax({
+    type: 'post',
+    url: $(this).attr('action'),
+    data: new FormData(this),
+    contentType: false,
+    cache: false,
+    processData: false,
+    success: function (result) {
+      $('#form-consult input').val("");
+      $('#thanks').modal('open');
+      localStorage.clear();
+    },
   });
 });
 
-// $(window).bind('mousewheel', function(event) {
-//   if (event.originalEvent.wheelDelta >= 0) {
-//       $('.navbar-fixed').attr("style","opacity: 1; transition: .5s; top: 0;").fadeIn(500);
-//   }
-//   else {
-//       $('.navbar-fixed').attr("style","opacity: 0; transition: .5s; top: -100px;");
-//   }
-//   });
-
-
-
+$('#phoneForm').mask('+375(00) 000-00-00');
